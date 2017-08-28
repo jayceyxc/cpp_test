db3afc0562c2e72c28913be9b040b2142c8e6525
Yuxuecheng <yuxuecheng@baicdata.com>
2017-08-28 10:22:07 +0800

    add the log

---
+++ include/log.h
@@ -0,0 +1,21 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+#define log_err(message) cerr << "file: " << __FILE__ << ", line: " << __LINE__ << ", function: " << __FUNCTION__ << ", compile time: " << __TIME__ << ", error message: " << message << endl
+
+/*
+void log_err(string message) {
+    cerr << "file: " << __FILE__ << ", line: " << __LINE__ << ", function: " << __FUNCTION__ << ", compile time: " << __TIME__ << ", error message: " << message << endl;
+}
+
+void log_err(const char* message) {
+    cerr << "file: " << __FILE__ << ", line: " << __LINE__ << ", function: " << __FUNCTION__ << ", compile time: " << __TIME__ << ", error message: " << message << endl;
+}
+*/
+
+#endif // LOG_H
